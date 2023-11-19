a8aac03c98802ff6dabac32d0f163c3cff7d7cd2
AndrewOliver616 <andre.oliveira.backup@outlook.com>
2023-11-19 00:39:51 -0300

    incluindo função de nível do jogador e texto de retorno de finalização do projeto

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 let usarName = "AndreFeraJS"
 let lostMatches = 22
 let winningMatches = 110
-let levelInGame = championLevel(winningMatches,lostMatches)
-//console de teste
-console.log("vitorias " + levelInGame)
+let levelInGame = championLevel(winningMatches, lostMatches)
+let finalRanked = rankedVictories (levelInGame)
+
 
 function championLevel(wMatches, lMatches) {
     return wMatches - lMatches 
 }
 
+function rankedVictories(levelInGame) {
+    if (levelInGame <10) {
+        levelPlayer = "Iron"
+    } else if (levelInGame<= 20) {
+        levelPlayer = "Bronze"
+    }else if (levelInGame<= 50) {
+        levelPlayer = "Silver"
+    }else if (levelInGame<= 80) {
+        levelPlayer = "Gold"
+    }else if (levelInGame<= 90) {
+        levelPlayer = "Diamond"
+    }else if (levelInGame<= 100) {
+        levelPlayer = "Legend"
+    }else if (levelInGame <= 101) {
+        levelPlayer = "Immortal"
+    }
 
+    return levelPlayer
+
+    
+}
+
+console.log(`O Herói tem de saldo de ${levelInGame} e está no nível ${finalRanked}`)
 
 
